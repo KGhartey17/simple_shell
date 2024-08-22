40e1ff674ca37b99555943b530b7cf97ed179e67
JK Ghartey <kaphaelghartey@gmail.com>
2024-08-22 12:24:13 -0700

    editting test file

--- test_shell.sh
+++ test_shell.sh
@@ -31,5 +31,55 @@ echo "echo nonexistentcommand" | ./hsh
 echo "Test 7: No fork for nonexistent command"
 echo "nonexistentcommand" | ./hsh
 
+# Test 8: Command with special characters
+echo "Test 8: Special characters in command"
+echo "echo 'Special chars: !@#$%^&*()'" | ./hsh
+
+# Test 9: Command with multiple arguments
+echo "Test 9: Command with multiple arguments"
+echo "echo 'Arg1 Arg2 Arg3'" | ./hsh
+
+# Test 10: Command with quotes
+echo "Test 10: Command with single quotes"
+echo "echo 'Single quoted argument'" | ./hsh
+echo "Test 10: Command with double quotes"
+echo "echo \"Double quoted argument\"" | ./hsh
+
+# Test 11: Command with environment variable
+echo "Test 11: Command with environment variable"
+export TEST_VAR="TestValue"
+echo "echo \$TEST_VAR" | ./hsh
+
+# Test 12: Empty command
+echo "Test 12: Empty command"
+echo "" | ./hsh
+
+# Test 13: Command with relative path
+echo "Test 13: Relative path"
+echo "./hsh" | ./hsh
+
+# Test 14: Command with trailing spaces
+echo "Test 14: Command with trailing spaces"
+echo "ls     " | ./hsh
+
+# Test 15: Command with multiple slashes in path
+echo "Test 15: Command with multiple slashes in path"
+echo "ls ///tmp" | ./hsh
+
+# Test 16: Long command argument
+echo "Test 16: Long command argument"
+LONG_ARG=$(head -c 1000 </dev/urandom | base64)
+echo "echo $LONG_ARG" | ./hsh
+
+# Test 17: Commands with invalid options
+echo "Test 17: Commands with invalid options"
+echo "ls --invalid-option" | ./hsh
+
+# Test 18: Test execution of a shell script
+echo "Test 18: Test execution of a shell script"
+echo -e "#!/bin/bash\n echo Script executed" > test_script.sh
+chmod +x test_script.sh
+echo "./test_script.sh" | ./hsh
+
 echo "All tests completed."
 
